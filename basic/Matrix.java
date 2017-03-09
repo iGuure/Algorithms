17498081a19003def961ea3bbd94369a3bccec61
Guure <iGuure>
2017-03-09 23:06:41 +0800

    Start the algorithm journey

---
+++ basic/Matrix.java
@@ -0,0 +1,30 @@
+/* 矩阵相乘（方阵） */
+public class Matrix {
+
+	public static void main(String[] args) {
+		int[][] a = {{1, 2}, {3, 4}};
+		int[][] b = {{1, 2}, {3, 4}};
+
+		int[][] c = new int[2][2];
+
+		int n = c.length;
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				for (int k = 0; k < n; k++) {
+					/* 关键：矩阵相乘的定义 */
+					c[i][j] += a[i][k] * b[k][j];
+				}
+			}
+		}
+
+		for (int[] i : c) {
+			for (int j : i) {
+				System.out.print(j + " ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
