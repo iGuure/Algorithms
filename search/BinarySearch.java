2e0daffac3c21af2e3064f0ca94148bc962783a7
Guure <iGuure>
2017-03-10 00:31:49 +0800

    Binary Search

---
+++ search/BinarySearch.java
@@ -0,0 +1,25 @@
+public class BinarySearch {
+    public static int rank(int key, int[] a) {
+        int lo = 0;
+        int hi = a.length - 1;
+        while (lo <= hi) {
+            // 被查找的键要么不存在，要么必然存在于a[lo..hi]之中
+            int mid = lo + (hi - lo) / 2;
+            if (key < a[mid])
+                hi = mid - 1;
+            else if (key > a[mid])
+                lo = mid + 1;
+            else
+                return mid;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1, 3, 5, 7, 9, 11};
+        int key = 5;
+        System.out.println(rank(key, a));
+        key = 8;
+        System.out.println(rank(key, a));
+    }
+}
