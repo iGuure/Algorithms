fe35b3fb0247757a25d0ffc4646cc62cd3966395
iGuure <543449492@qq.com>
2017-04-10 23:42:11 +0800

    added sort practice

---
+++ practice/SortPractice.java
@@ -0,0 +1,107 @@
+/* 排序算法练习 */
+import java.util.Collections;
+
+public class SortPractice {
+
+	public static void seletionSort(double[] d) {
+		/* 请在这里默写：选择排序 */
+	}
+
+	public static void insertionSort(double[] d) {
+		/* 请在这里默写：插入排序 */
+	}
+
+	public static void bubbleSort(double[] d) {
+		/* 请在这里默写：冒泡排序 */
+	}
+
+	public static void shellSort(double[] d) {
+		/* 请在这里默写：希尔排序 */
+	}
+
+	/* 请在这里默写：归并排序 */
+	public static double[] aux;
+
+	public static void mergeSort(double[] d) {
+	}
+
+	public static void mergeSort(double[] d, int lo, int hi) {
+	}
+
+	public static void merge(double[] d, int lo, int mid, int hi) {
+	}
+
+	/* 请在这里默写：快速排序 */
+	public static void quickSort(double[] d) {
+	}
+
+	public static void quickSort(double[] d, int lo, int hi) {
+	}
+
+	public static int partition(double[] d, int lo, int hi) {
+	}
+
+	public static void main(String[] args) {
+		
+		// 选择排序测试用例
+		// double d1[] = {34, 26, 74, 5, 20, 11};
+		// seletionSort(d1);
+
+		// System.out.print("选择排序：");
+		// for (double num : d1) {
+		// 	System.out.print(num + " ");
+		// }
+		// System.out.println();
+
+		// 插入排序测试用例
+		// double d2[] = {34, 26, 74, 5, 20, 11};
+		// insertionSort(d2);
+
+		// System.out.print("插入排序：");
+		// for (double num : d2) {
+		// 	System.out.print(num + " ");
+		// }
+		// System.out.println();
+
+		// 冒泡排序测试用例
+		// double d3[] = {34, 26, 74, 5, 20, 11};
+		// bubbleSort(d3);
+
+		// System.out.print("冒泡排序：");
+		// for (double num : d3) {
+		// 	System.out.print(num + " ");
+		// }
+		// System.out.println();
+
+		// 希尔排序测试用例
+		// double d4[] = {34, 26, 74, 5, 20, 11};
+		// shellSort(d4);
+
+		// System.out.print("希尔排序：");
+		// for (double num : d4) {
+		// 	System.out.print(num + " ");
+		// }
+		// System.out.println();
+
+		// 归并排序测试用例
+		// double d5[] = {34, 26, 74, 5, 20, 11};
+		// mergeSort(d5);
+
+		// System.out.print("归并排序：");
+		// for (double num : d5) {
+		// 	System.out.print(num + " ");
+		// }
+		// System.out.println();
+
+		// 快速排序测试用例
+		// double d6[] = {34, 26, 74, 5, 20, 11};
+		// quickSort(d6);
+
+		// System.out.print("快速排序：");
+		// for (double num : d6) {
+		// 	System.out.print(num + " ");
+		// }
+		// System.out.println();
+
+	}
+}
